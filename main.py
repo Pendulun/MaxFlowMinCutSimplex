338e11b92995859ebb1ed971622b4a754eef4eea
Daniel Campos <danielcampos@dcc.ufmg.br>
2021-08-19 20:05:19 -0300

    Populando vetor b da PL

--- main.py
+++ main.py
@@ -24,22 +24,34 @@ def popular_matriz_restricoes(matriz_incidencia):
     """A matriz de restrições é formada por todas as linhas da matriz_incidencia
     relativas a vértices que não sejam de "s" e nem de "t" além de ter uma matriz 
     identidade do tamanho do número de arestas
+    |  M  | 
+    |  I  |
     """
     linhas_vertices = matriz_incidencia[1:-1].copy()
     num_arestas = matriz_incidencia.shape[1]
     restricoes = np.concatenate((linhas_vertices, np.identity(num_arestas)), axis=0)
     return restricoes
 
+def popular_vetor_b(num_vertices_intermediarios, capacidades):
+    """
+    O vetor b da PL será formado por entradas 1's relativas aos vértices que não sejam
+    "s" nem "t" seguido das capacidades máximas de cada vértice
+    |   1's |
+    | cap's |
+    """
+    vetor_uns = np.ones(num_vertices_intermediarios)
+    b = np.concatenate((vetor_uns, capacidades), axis = 0)
+    return b
 
 def main():
     
     capacidades,matriz_incidencia=leEntrada()
     c = popular_vetor_c(matriz_incidencia)
-    #popular matriz restricoes
+    print("Vetor C:\n{}".format(c))
     restricoes = popular_matriz_restricoes(matriz_incidencia)
-    #popular vetor b
-    #Vetor b é formado por 1 para 
-    
+    print("Matriz Restricoes:\n{}".format(restricoes))
+    b = popular_vetor_b(matriz_incidencia.shape[0]-2, capacidades)
+    print("Vetor b gerado:\n{}".format(b))
 
     """
     #popular b
