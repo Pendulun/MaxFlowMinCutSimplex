34e9e0afa7604c2f22b59155da8e3f83a87874fc
Daniel Campos <danielcampos@dcc.ufmg.br>
2021-08-19 19:30:32 -0300

    Populando vetor c da PL

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ def leEntrada():
 
     num_vertices = int(num_vertices)
     num_arestas = int(num_arestas)
-    matriz_incidencia = np.zeros((num_vertices,num_arestas), dtype="float32")
-    capacidades = np.array(input().split(), dtype="float32")
+    matriz_incidencia = np.zeros((num_vertices,num_arestas), dtype="int16")
+    capacidades = np.array(input().split(), dtype="int32")
 
     for i in range(num_vertices):
         matriz_incidencia[i] = np.array(list(input().split()))
 
     return capacidades,matriz_incidencia
 
+def popular_vetor_c(matrizIncidencia):
+    return matrizIncidencia[0]
+
 def main():
     
     capacidades,matriz_incidencia=leEntrada()
-    
+    c = popular_vetor_c(matriz_incidencia)
+    #popular vetor b
+
     """
     #popular b
     my_simplex = Simplex(c,b,restricoes)
